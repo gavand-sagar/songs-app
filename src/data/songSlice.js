dcfbbf927016292df2b8bdbc179b6f4a029a823d
gavand-sagar <gavand.sagar.s@gmail.com>
2023-03-24 14:20:44 +0530

    redux thunk

--- src/data/songSlice.js
+++ src/data/songSlice.js
@@ -1,17 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getJsonData } from "../shared/utils/ApiUtitilities.js";
+
+export const getAllSongs = createAsyncThunk('getAllSongs', () => {
+    return getJsonData('/songs')
+})
+
+export const getThoughts = createAsyncThunk('getThoughts', () => {
+    return getJsonData('/thoughts')
+})
+
+export const getProducts = createAsyncThunk('getProducts', () => {
+    return getJsonData('/products')
+})
+
 
 const songsSlice = createSlice({
     name: 'songs',
     initialState: {
-        allSongs: []
+        loadingTheSongs: false,
+        allSongs: [],
+        thoughts: [],
+        products: []
     },
-    reducers: {
-        changeAllSongs: (state, action) => {
+    extraReducers: (builder) => {
+
+        //this is what should happen when server will send you response 
+        // response will be available in (action.payload)
+        builder.addCase(getAllSongs.pending, (state, action) => {
+            state.loadingTheSongs = true;
+        })
+
+        //this is what should happen when server will send you response 
+        // response will be available in (action.payload)
+        builder.addCase(getAllSongs.fulfilled, (state, action) => {
             state.allSongs = action.payload
-        }
+            state.loadingTheSongs = false;
+
+        })
+
+
+        //this is what should happen when server will send you response 
+        // response will be available in (action.payload)
+        builder.addCase(getThoughts.fulfilled, (state, action) => {
+            state.thoughts = action.payload
+        })
+
+
+        //this is what should happen when server will send you response 
+        // response will be available in (action.payload)
+        builder.addCase(getProducts.fulfilled, (state, action) => {
+            state.products = action.payload
+        })
+
     }
+    // reducers: {
+    //     changeAllSongs: (state, action) => {
+    //         state.allSongs = action.payload
+    //     }
+    // }
 })
 
-export const { changeAllSongs } = songsSlice.actions;
+// export const { changeAllSongs } = songsSlice.actions;
 
 export default songsSlice;
--- src/features/SongsLists/SongsList.js
+++ src/features/SongsLists/SongsList.js
@@ -1,43 +1,28 @@
 import React, { useState } from 'react'
-import { getJsonData } from '../../shared/utils/ApiUtitilities.js'
 import Header from '../../shared/components/Header/Header.js';
 import SongItem from './SongItem.js';
 import SongSkeleton from './SongSkeleton.js';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeAllSongs } from '../../data/songSlice.js';
+import { getAllSongs } from '../../data/songSlice.js';
 
 export default function SongsList() {
 
-    // const [songs, setSongs] = useState([])
-
     //redux store value for the song
     const { allSongs } = useSelector(state => state.songs)
-
-    let { username } = useSelector(state => state.user)
-
-    const [isLoading, setIsLoading] = useState(false);
-
+    const { loadingTheSongs } = useSelector(state => state.songs)
     const dispatch = useDispatch();
-
     function getData() {
-        // try to set it from redux store
-        // api call
-        setIsLoading(true)
-        getJsonData('/songs')
-            .then(response => {
-                setIsLoading(false)
-                dispatch(changeAllSongs(response))
-            })
+        dispatch(getAllSongs())
     }
 
     return (
         <div>
             <Header />
-            <h1>SongsList  {username}</h1>
+            <h1>SongsList</h1>
             <button onClick={getData}>LoadSongs</button>
             <hr />
             {
-                isLoading == true ?
+                loadingTheSongs == true ?
                     <div>
                         <SongSkeleton />
                         <SongSkeleton />
